105406cb006e5ae1fcae8f3017f74e662d105f9f
Quan <hmquan3@cmcglobal.vn>
2025-07-12 18:14:26 +0700

    config turborepo

--- services/habit/src/main.ts
+++ services/habit/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4002);
 }
 bootstrap();
--- services/habit/src/shared/prisma.service.ts
+++ services/habit/src/shared/prisma.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, type OnModuleInit } from '@nestjs/common';
 import { type Challenge, Prisma, PrismaClient } from '@prisma-client/habit';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit(): Promise<void> {
